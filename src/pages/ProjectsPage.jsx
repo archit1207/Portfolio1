29096f5c5503ec4ae6315aeb68f20cd7effa349c
archit1207 <architmishra1207@gmail.com>
2026-04-09 23:47:42 +0530

    Fix project data import path

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import BackgroundSVG from "../components/BackgroundSVG";
 import GlowBlobs from "../components/GlowBlobs";
-import projectsData from "../data/Projectdata";
+import projectsData from "../data/projectsData";
 
 const projects = projectsData;
 
